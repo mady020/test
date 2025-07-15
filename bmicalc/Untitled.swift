9a7ebe7c37813bde8bb2e8650b57a9d975b93a8f
mady020 <madhav1887.be23@chitkara.edu.in>
2025-07-15 11:31:51 +0530

    outlets and actions
    i got the idea, i need to implement this and make a small app to understand more deeply

---
+++ bmicalc/Untitled.swift
@@ -0,0 +1,7 @@
+//
+//  Untitled.swift
+//  bmicalc
+//
+//  Created by Student on 15/07/25.
+//
+
--- bmicalc/bmicalc/ViewController.swift
+++ bmicalc/bmicalc/ViewController.swift
@@ -1,35 +1,63 @@
-//
-//  ViewController.swift
-//  bmicalc
-//
-//  Created by Student on 14/07/25.
-//
-
 import UIKit
+
+struct Result {
+    var weight: Double
+    var height: Double
+    var bmi: Double {
+        return weight / (height * height)
+    }
+}
 
 class ViewController: UIViewController {
     @IBOutlet weak var weightTextField: UITextField!
     @IBOutlet weak var heightTextField: UITextField!
     @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var historyText: UITextView!
+ 
+
+    
+    @IBOutlet weak var historyLabel: UILabel!
+    var results: [Result] = []
+
     @IBAction func calculateButton(_ sender: Any) {
-        guard let
-                weight = Double(weightTextField.text ?? "") else{
+  
+        guard let weightText = weightTextField.text, let weight = Double(weightText), weight > 0 else {
             resultLabel.text = "Invalid Weight!"
             return
         }
-        guard let
-                height = Double(heightTextField.text ?? "") else{
+     
+        guard let heightText = heightTextField.text, let height = Double(heightText), height > 0 else {
             resultLabel.text = "Invalid Height!"
             return
         }
-        let bmi = weight / (height * height)
-        resultLabel.text = "Bmi: " + String(bmi)
+
+        let result = Result(weight: weight, height: height)
+        results.append(result)
+
+
+        resultLabel.text = "BMI: " + String(format: "%.2f", result.bmi)
+
+        updateHistory()
     }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateHistory()
+       
     }
 
-
+    func updateHistory() {
+        if results.isEmpty {
+            historyLabel.text = "No Result Found!"
+        } else {
+            historyLabel.text = "History"
+            var historyString:String = ""
+            for result in results {
+                historyString += "Height: \(result.height), Weight: \(result.weight), BMI: \(String(format: "%.2f", result.bmi))\n"
+            }
+            print(historyString)
+            historyText.text = historyString
+        }
+    }
 }
 
