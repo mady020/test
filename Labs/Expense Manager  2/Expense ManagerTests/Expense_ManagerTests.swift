5305cf74289ac615be40814fdc60e94431bbbbec
mady020 <madhav1887.be23@chitkara.edu.in>
2025-07-19 16:06:21 +0530

    similar app using installed option

---
+++ Labs/Expense Manager  2/Expense ManagerTests/Expense_ManagerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Expense_ManagerTests.swift
+//  Expense ManagerTests
+//
+//  Created by Student on 17/07/25.
+//
+
+import Testing
+@testable import Expense_Manager
+
+struct Expense_ManagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Labs/blogApp/blogApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  blogApp
+//
+//  Created by Student on 19/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
