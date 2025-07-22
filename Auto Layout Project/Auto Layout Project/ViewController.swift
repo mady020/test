820033d82d48342eca386ccc757519c11893de5e
mady020 <madhav1887.be23@chitkara.edu.in>
2025-07-22 09:15:20 +0530

    tab bar controller and tab views

---
+++ Auto Layout Project/Auto Layout Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Auto Layout Project
+//
+//  Created by Student on 21/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
