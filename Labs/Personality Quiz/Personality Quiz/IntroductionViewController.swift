60021867c32703e16a18230e95fb4002ccb3096b
mady020 <madhav1887.be23@chitkara.edu.in>
2025-07-23 11:44:45 +0530

    Personality Quiz App
    done 30% to 40%

---
+++ Labs/Personality Quiz/Personality Quiz/IntroductionViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Personality Quiz
+//
+//  Created by Student on 23/07/25.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
