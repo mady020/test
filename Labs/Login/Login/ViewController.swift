4bb266a0807add43e9ab6b099083558b7ea6b70f
mady020 <madhav1887.be23@chitkara.edu.in>
2025-07-23 09:59:51 +0530

    segues in login page works
    personality quiz next

--- Labs/Login/Login/ViewController.swift
+++ Labs/Login/Login/ViewController.swift
@@ -8,13 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var password: UITextField!
     @IBOutlet weak var username: UITextField!
     override func viewDidLoad() {
+        infoLabel.text = ""
         super.viewDidLoad()
     }
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        segue.destination.navigationItem.title = "Welcome, \(username.text ?? "User")!"
+    @IBAction func loginButtonPressed(_ sender: UIButton) {
+        if username.text == "mady" && password.text == "pass" {
+            performSegue(withIdentifier: "changeView", sender: nil)
+        }else {
+            infoLabel.textColor = .red
+            infoLabel.text = "Invalid Email or password!"
+        }
     }
-    
+    @IBOutlet var infoLabel: UILabel!
+    @IBOutlet var password: UITextField!
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "forgotUsername" {
+            segue.destination.navigationItem.title = "Forgot Username"
+        }else if segue.identifier == "forgotPassword"{
+            segue.destination.navigationItem.title = "Forgot Password"
+        }else if segue.identifier == "changeView" {
+            let name = username.text?.trimmingCharacters(in: .whitespacesAndNewlines)
+            segue.destination.navigationItem.title = "Welcome, \(name ?? "User")!"
+        }
+    }
 }
